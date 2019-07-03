02bbba171238c8a296a57b50887832e2ca8c7592
Marcel Wege <mwege@byte5.de>
2019-07-03 13:48:06 +0200

    Add Title Tag

--- resources/views/project/new.blade.php
+++ resources/views/project/new.blade.php
@@ -10,6 +10,10 @@
             <div class="col-12">
                 <form action="{{route('project.store')}}" method="post">
                     @csrf
+                    <div class="form-group">
+                        <label for="title">Titel</label>
+                        <input type="text" id="title" name="title" class="form-control" />
+                    </div>
                     <button class="btn btn-danger" type="submit">Abschicken</button>
                 </form>
             </div>
